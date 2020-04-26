a6ef0ab37605f931c4bead31e4c2e214178dcb0e
Ranieri Valença <ranierivalenca@gmail.com>
2020-04-26 18:15:13 -0300

    New ADL specs suggestion

--- AMoTAdl.py
+++ AMoTAdl.py
@@ -11,7 +11,25 @@ Attachments = {
     'Requestor': 'ClientRequestHandler'
 }
 
+Functions = {
+    # 'publisher',
+    'subscriber'
+}
+
+SubscriberTopics = {
+    'hello'
+}
+
+SubscriberTimeLimit = 5000
+
+Configs = {
+    'serverIP': '192.168.0.102',
+    'serverPort': 60000,
+    'listenPort': 60001,
+}
+
 Starter = {
+    #'AMoTPublisher', 'ServerRequestHandler'
     'AMoTClient'
 }
 
