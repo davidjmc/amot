e709c0c31b4b40c384809d1d3b6cded7c0466e47
Ranieri Valença <ranierivalenca@gmail.com>
2020-04-26 18:14:49 -0300

    Removing the loop from ServerRequestHandler

--- middleware/ServerRequestHandler.py
+++ middleware/ServerRequestHandler.py
@@ -13,25 +13,24 @@ class ServerRequestHandler(Component):
         self.message = None
 
     def run(self, *args):
-        while True:
-            try:
-                if self.server_sock is None:
-                    self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    self.address = socket.getaddrinfo(amot_broker_ip, amot_broker_port)[0][-1]
+        try:
+            if self.server_sock is None:
+                self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.address = socket.getaddrinfo(amot_broker_ip, amot_broker_port)[0][-1]
 
-                    try:
-                        self.server_sock.bind(self.address)
-                        print('<Starting up on {} port {}'.format(*self.address))
-                        self.server_sock.listen(5)
-                    except OSError as e:
-                        print('ServerRequestHandler Bind Error: ', e)
+                try:
+                    self.server_sock.bind(self.address)
+                    print('<Starting up on {} port {}'.format(*self.address))
+                    self.server_sock.listen(5)
+                except OSError as e:
+                    print('ServerRequestHandler Bind Error: ', e)
 
-            except OSError as e:
-                print('ServerRequestHandler Socket Error: ', e)
+        except OSError as e:
+            print('ServerRequestHandler Socket Error: ', e)
 
-            self.receive()
-            self.external().run(self.message)
+        self.receive()
+        self.external().run(self.message)
 
     def receive(self):
         buffer_size = 536
