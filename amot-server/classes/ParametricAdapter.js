dd0b0db92a71e1f7f9d3018c2202138a1679b8cc
David <david.junio@gmail.com>
2021-07-16 18:18:23 -0300

    Add json-rules-engine of the parametric adapter

--- amot-server/classes/ParametricAdapter.js
+++ amot-server/classes/ParametricAdapter.js
@@ -1,5 +1,6 @@
 const Adapter = require('./Adapter')
 const Adaptation = require('./Adaptation')
+const {Engine} = require('json-rules-engine')
 
 class ParametricAdapter extends Adapter {
     constructor(thing) {
@@ -19,28 +20,120 @@ class ParametricAdapter extends Adapter {
     // TODO - be generic
     async monitor(request) {
         return {
-            temperature: parseInt(request.headers['temperature'])
+            volume: parseFloat(request.headers['fluid_volume']),
+            capacity: parseFloat(request.headers['reservoir_capacity'])
         }
     }
 
     // return the "command" for the correct condition based on variables
     // TODO - be generic
     async analyzer(variables) {
-        let temperature = variables.temperature
-        console.log(temperature)
-        if (temperature > 70) {
-            return {
-                sleep_time: 5
-            }
-        } else if (temperature > 50) {
-            return {
-                sleep_time: 3
-            }
-        } else if (temperature > 0) {
-            return {
-                sleep_time: 1
+        const engine = new Engine()
+
+        const dangerLevel = {
+            conditions: {
+                all: [{
+                    fact: 'volumeInformation',
+                    operator: 'lessThanInclusive',
+                    value: 25
+                }]
+            },
+            event: {
+                type: 'change-collection-frequency',
+                params: {
+                    sleep_time: 5 //min
+                }
             }
         }
+        engine.addRule(dangerLevel)
+
+
+        const notsafeLevel = {
+            conditions: {
+                all: [{
+                    fact: 'volumeInformation',
+                    operator: 'greaterThan',
+                    value: 25
+                }, {
+                    fact: 'volumeInformation',
+                    operator: 'lessThanInclusive',
+                    value: 50
+                }]
+            },
+            event: {
+                type: 'change-collection-frequency',
+                params: {
+                    sleep_time: 10 //min
+                }
+            }
+
+        }
+        engine.addRule(notsafeLevel)
+
+
+        const safeLevel = {
+            conditions: {
+                all: [{
+                    fact: 'volumeInformation',
+                    operator: 'greaterThan',
+                    value: 50
+                }, {
+                    fact: 'volumeInformation',
+                    operator: 'lessThanInclusive',
+                    value: 75
+                }]
+            },
+            event: {
+                type: 'change-collection-frequency',
+                params: {
+                    sleep_time: 15 //min
+                }
+            }
+
+        }
+        engine.addRule(safeLevel)
+
+
+        const peaceLevel = {
+            conditions: {
+                all: [{
+                    fact: 'volumeInformation',
+                    operator: 'greaterThan',
+                    value: 75
+                }]
+            },
+            event: {
+                type: 'change-collection-frequency',
+                params: {
+                    sleep_time: 30 //min
+                }
+            }
+        }
+        engine.addRule(peaceLevel)
+
+
+        let fact = ( (variables.volume * 100.0) / variables.capacity )
+
+        let facts = {volumeInformation: fact}
+        var ret = await engine.run(facts)
+        return ret.events[0].params
+
+        //let volume = variables
+        //console.log("ENTRO AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+        //console.log(volume)
+        //if (temperature > 70) {
+        //    return {
+        //        sleep_time: 5
+        //    }
+        //} else if (temperature > 50) {
+        //    return {
+        //        sleep_time: 3
+        //    }
+        //} else if (temperature > 0) {
+        //    return {
+        //        sleep_time: 1
+        //    }
+        //}
     }
 
     planner(changes) {
